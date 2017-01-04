5f65dcad29ea01dd12b5f14859ff2c76b82ca7fd
Jonathan Islas <islas.27@gmail.com>
2017-01-04 12:30:25 -0700

    Added an example of cat() usage

---
+++ examples/cat.js
@@ -0,0 +1,23 @@
+let StringHelper = require('../index')
+
+let helper = new StringHelper()
+let result = helper.cat('this is the first line', '\n')
+    .cat('here is the second\n')
+    .cat(['and then', ' the third'], '\n')
+    .cat('now').cat(function () {
+      return ' we can make some calcs '
+    }).cat(function () {
+      return ['or even do more complex mixups', function () { return ' here\n' }]
+    }).cat(function () {
+      this.cat('like using all the helper functions in a function')
+    })
+    .cat(' inside a cat()').str()
+
+console.log(result)
+
+// Output:
+// this is the first line
+// here is the second
+// and then the third
+// now we can make some calcs or even do more complex mixups here
+// like using all the helper functions in a function inside a cat()
