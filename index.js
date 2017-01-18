45aa5738969e4f51eb00f9f76d6637761ecf0522
Jonathan Islas <islas.27@gmail.com>
2017-01-18 10:59:35 -0700

    Added method wrap()
    — added unit test
    — WIP: breaks a test in cat (helper available inside a function passed as a parameter)
    - Issue #5

--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const isType = (type, inp) => typeof inp === type
 const isFunc = inp => isType('function', inp)
 const isNum = inp => isType('number', inp)
 const isIgnored = inp => isType('undefined', inp) || isType('object', inp)
+const wrp = (pf, arg, sf) => pf.concat([...arg].concat(sf))
 
 function stringHelper () {
+  let aux = []
   let buffer = []
+  let prefixes = []
+  let suffixes = []
+  let decorators = false
+  let that = this
+
+  function ccat () {
+    const len = arguments.length
+    let args = arguments
+    for (let i = 0; i < len; i += 1) {
+      let arg = args[i]
+      if (isFunc(arg)) ccat(arg.apply(that))
+      else if (isArr(arg)) ccat(...arg.flatten())
+      else if (isIgnored(arg)) continue
+      else aux.push(arg)
+    }
+  }
 
   this.cat = function () {
-    const len = arguments.length
-    for (let i = 0; i < len; i += 1) {
-      let arg = arguments[i]
-      if (isFunc(arg)) this.cat(arg.apply(this))
-      else if (isArr(arg)) this.cat(...arg.flatten())
-      else if (isIgnored(arg)) continue
-      else buffer.push(arg)
-    }
+    let args = (decorators) ? wrp(prefixes, arguments, suffixes) : arguments
+    aux = []
+    ccat([...args])
+    buffer.push(...aux)
     return this
   }
 
@@ -40,6 +54,14 @@ function stringHelper () {
         this.cat(args)
       }
     } else this.cat(args.concat(rep))
+    return this
+  }
+
+  this.wrap = function (prefix, suffix) {
+    if ([...arguments].length !== 2) return this
+    decorators = true
+    prefixes.unshift(prefix)
+    suffixes.push(suffix)
     return this
   }
 
---
+++ test/wrap.spec.js
@@ -0,0 +1,55 @@
+/* eslint-env mocha */
+const StringHelper = require('../index')
+const expect = require('chai').expect
+const sinon = require('sinon')
+
+describe('#wrap', () => {
+  it('ignores calls with no arguments', () => {
+    let helper = new StringHelper('Hello')
+    let result = helper.wrap().str()
+    expect(result).to.equal('Hello')
+  })
+
+  it('ignores calls with missing arguments', () => {
+    let helper = new StringHelper('Hello')
+    let result = helper.wrap(' Stranger').str()
+    expect(result).to.equal('Hello')
+  })
+
+  it('ignores calls with more arguments than indicated', () => {
+    let helper = new StringHelper('Hello')
+    let result = helper.wrap(' Stranger!', ' Who', ' Are you?').str()
+    expect(result).to.equal('Hello')
+  })
+
+  it('appends correctly the decorators to cat() calls', () => {
+    let helper = new StringHelper()
+    let result = helper.wrap('1', '3').cat('2').str()
+    expect(result).to.equal('123')
+  })
+
+  it('affects only the posterior cat() calls', () => {
+    let helper = new StringHelper('0')
+    let result = helper.wrap('1', '3').cat('2').wrap('2', '4').cat('').str()
+    expect(result).to.equal('01232134')
+  })
+
+  it('is capable of receiving functions', () => {
+    let helper = new StringHelper()
+    let result = helper.cat('<ul>').wrap(['<li>', (function () {
+      var count = 0
+      return function () {
+        count += 1
+        return count
+      }
+    }()), '.- '], '</li>')
+    .rep('list item', 2).str()
+    expect(result).to.equal(`<ul><li>1.- list item</li><li>2.- list item</li>`)
+  })
+
+  it('is capable of receiving more complex inputs', () => {
+    let helper = new StringHelper()
+    let result = helper.wrap(['1', '2', '3'], ['5', '6', '7']).cat('4').str()
+    expect(result).to.equal('1234567')
+  })
+})
